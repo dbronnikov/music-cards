42083a104dc57ef5e9fe439d67528b7b64c2f90b
Dennis Bronnikov <bronnikov.dennis@gmail.com>
2023-05-22 15:11:45 -0700

    Update haplaylist.sh

--- haplaylist.sh
+++ haplaylist.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 playlist=$1
 
-curl --max-time 2 --silent --output /dev/null -X POST http://localhost:5005/Sonos%20Big%20Roam/play/$playlist
+echo "DEBUG: curl -X GET http://localhost:5005/Big%20Move/playlist/$playlist"
+curl -X GET http://localhost:5005/Big%20Move/playlist/$playlist
